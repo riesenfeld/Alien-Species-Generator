e9f5eaec6cb5906d017452ebd0f235d34abf9089
riesenfeld <aron.riesenfeld@gmail.com>
2022-10-12 12:29:57 -0700

    basic randomization and info display functionality

---
+++ components/InformationCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-sheet id="info-card" :color="color">
+    <section>
+      <h2>{{ content.main.name }}</h2>
+      <p>{{ content.main.description }}</p>
+    </section>
+    <section v-if="content.additions">
+      <h3>{{ content.additions.name }}</h3>
+      <p>{{ content.additions.description }}</p>
+    </section>
+    <section v-if="content.styles">
+      <h4>{{ content.styles.name }}</h4>
+      <p>{{ content.styles.description }}</p>
+    </section>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: 'InformationCard',
+  props: {
+    color: {
+      type: String,
+      default: 'white',
+    },
+    content: {
+      type: Object,
+      default: () => {
+        return {
+          main: { name: 'Generating...', description: '' },
+          additions: false,
+          styles: false,
+        }
+      },
+    },
+    //  additions: {
+    //    type: Object,
+    //    default: () => {
+    //      return { name: '', description: '' }
+    //    },
+    //  },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#info-card {
+  color: black;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
+  <v-row justify="center" align="center" width="100%" height="100%">
+    <v-col cols="12" sm="6" md="6">
+      <InformationCard
+        color="red"
+        :content="pickRandomParentingStyle()"
+      ></InformationCard>
+      <InformationCard
+        color="yellow"
+        :content="pickRandomReproduction()"
+      ></InformationCard>
+    </v-col>
+    <v-col cols="12" sm="6" md="6">
+      <InformationCard
+        color="green"
+        :content="pickRandomAnatomy()"
+      ></InformationCard>
+      <InformationCard
+        color="blue"
+        :content="pickRandomDiet()"
+      ></InformationCard>
+    </v-col>
+    <!-- <v-card class="logo py-4 d-flex justify-center">
         <NuxtLogo />
         <VuetifyLogo />
-      </v-card>
-      <v-card>
+      </v-card> -->
+    <!-- <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
         </v-card-title>
@@ -75,13 +94,79 @@
           <v-spacer />
           <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
         </v-card-actions>
-      </v-card>
-    </v-col>
+      </v-card> -->
+    <!-- </v-col> -->
   </v-row>
 </template>
 
 <script>
+import InformationCard from '@/components/InformationCard.vue'
+import speciesDataJSON from '@/data/species_data.json'
+
 export default {
   name: 'IndexPage',
+  components: { InformationCard },
+  data() {
+    return {
+      speciesData: speciesDataJSON,
+    }
+  },
+  methods: {
+    pickRandomParentingStyle() {
+      const randIndex = Math.floor(
+        Math.random() * this.speciesData.PARENTING_STYLE.length
+      )
+      return {
+        main: this.speciesData.PARENTING_STYLE[randIndex],
+        additions: false,
+        styles: false,
+      }
+    },
+    pickRandomReproduction() {
+      const randMainInfoIndex = Math.floor(
+        Math.random() * this.speciesData.REPRODUCTION.length
+      )
+      const randAdditionsIndex = Math.floor(
+        Math.random() * this.speciesData.REPRODUCTION_ADDITIONS.length
+      )
+
+      return {
+        main: this.speciesData.REPRODUCTION[randMainInfoIndex],
+        additions: this.speciesData.REPRODUCTION_ADDITIONS[randAdditionsIndex],
+        styles: false,
+      }
+    },
+    pickRandomAnatomy() {
+      const randMainInfoIndex = Math.floor(
+        Math.random() * this.speciesData.ANATOMY.length
+      )
+      const randAdditionsIndex = Math.floor(
+        Math.random() * this.speciesData.ANATOMY_ADDITIONS.length
+      )
+
+      return {
+        main: this.speciesData.ANATOMY[randMainInfoIndex],
+        additions: this.speciesData.ANATOMY_ADDITIONS[randAdditionsIndex],
+        styles: false,
+      }
+    },
+    pickRandomDiet() {
+      const randMainInfoIndex = Math.floor(
+        Math.random() * this.speciesData.DIET.length
+      )
+      const randAdditionsIndex = Math.floor(
+        Math.random() * this.speciesData.DIET_ADDITIONS.length
+      )
+      const randStylesIndex = Math.floor(
+        Math.random() * this.speciesData.DIET_STYLES.length
+      )
+
+      return {
+        main: this.speciesData.DIET[randMainInfoIndex],
+        additions: this.speciesData.DIET_ADDITIONS[randAdditionsIndex],
+        styles: this.speciesData.DIET_STYLES[randStylesIndex],
+      }
+    },
+  },
 }
 </script>
