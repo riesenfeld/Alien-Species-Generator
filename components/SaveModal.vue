735293cb1df0a69abdda065a13f761d4f0560887
riesenfeld <aron.riesenfeld@gmail.com>
2022-10-15 17:07:55 -0700

    removed superfluous v-row

--- components/SaveModal.vue
+++ components/SaveModal.vue
@@ -1,48 +1,45 @@
 <template>
-  <v-row justify="center">
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <template #activator="{ on, attrs }">
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template #activator="{ on, attrs }">
+      <v-btn
+        class="btn"
+        :tile="!isNarrow"
+        :rounded="isNarrow"
+        :color="pdfSaved ? 'success' : 'primary'"
+        v-bind="attrs"
+        v-on="on"
+        >{{ isNarrow ? '' : pdfSaved ? 'PDF saved!&nbsp;' : 'Save as PDF&nbsp;'
+        }}<v-icon>mdi-file-pdf-box</v-icon></v-btn
+      >
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">Give this species a name</span>
+      </v-card-title>
+      <v-card-text>
+        <v-text-field
+          v-model="speciesName"
+          label="Name"
+          hint="the name you want to give this species"
+          required
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">
+          Never mind
+        </v-btn>
         <v-btn
-          class="btn"
-          :tile="!isNarrow"
-          :rounded="isNarrow"
-          :color="pdfSaved ? 'success' : 'primary'"
-          v-bind="attrs"
-          v-on="on"
-          >{{
-            isNarrow ? '' : pdfSaved ? 'PDF saved!&nbsp;' : 'Save as PDF&nbsp;'
-          }}<v-icon>mdi-file-pdf-box</v-icon></v-btn
+          color="blue darken-1"
+          text
+          :disabled="speciesName.trim().length <= 0"
+          @click="handleConfirm"
         >
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="text-h5">Give this species a name</span>
-        </v-card-title>
-        <v-card-text>
-          <v-text-field
-            v-model="speciesName"
-            label="Name"
-            hint="the name you want to give this species"
-            required
-          ></v-text-field>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">
-            Never mind
-          </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            :disabled="speciesName.trim().length <= 0"
-            @click="handleConfirm"
-          >
-            Confirm
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+          Confirm
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
